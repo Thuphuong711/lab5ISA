6b7bcbc55a9e386a4683bba439b38f7829952d77
Le Thi Thu Phuong <lttphuong7@gmail.com>
2025-02-15 11:36:26 -0800

    fix CORS

--- server.js
+++ server.js
@@ -7,9 +7,14 @@ const messages = require('./lang/en/en');
 
 
 const server = http.createServer((req,res) => {
-    res.setHeader('Access-Control-Allow-Origin', "*");
-    res.setHeader('Access-Control-Allow-Methods', 'GET', 'POST');
-    res.setHeader('Content-Type', "application/json");
+    // res.setHeader('Access-Control-Allow-Origin', "*");
+    // res.setHeader('Access-Control-Allow-Methods', 'GET', 'POST');
+    // res.setHeader('Content-Type', "application/json");
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');  // Ensure OPTIONS is included for preflight requests
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');  // Allow necessary headers
+    res.setHeader('Content-Type', 'application/json');
 
     if(req.method === "POST" && req.url.startsWith("/insert")){
         // Insert a new patient recorrd
