eadbeedb74b32f50abba0555962988a1ba5e885d
Le Thi Thu Phuong <lttphuong7@gmail.com>
2025-02-16 08:43:23 -0800

    test get all query

--- test-db.js
+++ test-db.js
@@ -1,6 +1,6 @@
 const connection = require('./db_config');
 
-connection.query("SELECT 1", (err, result) => {
+connection.query("SELECT * FROM patient", (err, result) => {
     if (err) {
         console.error("Database Connection Failed:", err);
     } else {
